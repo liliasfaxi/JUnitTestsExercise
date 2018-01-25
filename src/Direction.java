1f804743e272209d40b686f15d8c7419e8633f49
Belguith Omar <omar.belguith@medtech.tn>
2018-01-25 10:01:25 +0100

    removed .idea  and added new tests

--- src/Direction.java
+++ src/Direction.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
 
 public class Direction {
-    int x = 0, y=0;
+    public int x = 0, y=0;
 
     public void deplacer(char c){
         switch (c){
             case 'N' : y++;
+            break;
             case 'O' : x--;
-            case 'S' : x++;
+            break;
+            case 'S' : y--;
+            break;
+            case 'E': x++;
+            break;
         }
     }
 
